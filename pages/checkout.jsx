dd6d886ef387082a638c78290d992a392b3b2b2f
maher <gomemahero.com>
2022-06-25 23:38:01 +0300

    payment start work good

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import PhoneInput from "react-phone-input-2";
-//import getStripe from '../../lib/getStripe';
-
+import { toast } from "react-toastify";
+import getStripe from '../lip/getStripe';
 
 import {
   query,
@@ -10,6 +10,7 @@ import {
   collection,
   doc,
   setDoc,
+  updateDoc,
   addDoc,
 } from "firebase/firestore";
 import {
@@ -17,11 +18,13 @@ import {
   useDocumentData,
 } from "react-firebase-hooks/firestore";
 import { db } from "../firebase";
-import { globaluse} from "../context/global";
+import { globaluse } from "../context/global";
 import { get } from "react-hook-form";
 const Step2 = () => {
   const { userinfo } = globaluse();
+  const [ordercheckout, setOrdercheckout] = useState(true);
   const [userdata] = useDocumentData(doc(db, "Users", `${userinfo?.email}`));
+  const [orderinfo] = useDocumentData(doc(db, "orders2", `${userinfo?.email}`));
 
   const [values, setValues] = useState({
     firstname: "",
@@ -55,9 +58,9 @@ const Step2 = () => {
 
     const docRef =
       // await addDoc(collection(db, "orders",  `${userinfo?.email}`)
-      await setDoc(doc(db, "orders", `${userinfo?.email}`), {
+      await setDoc(doc(db, "orders2", `${userinfo?.email}`), {
         user: values.email,
-       
+
         firstname: values.firstname,
         lastname: values.lastname,
         phone: values.phone,
@@ -69,32 +72,75 @@ const Step2 = () => {
         status: "pending",
         cartitems: userdata?.cart,
         total: userdata?.totalprice,
+      }).then(() => {
+        toast.success("Order Placed Successfully");
+        setOrdercheckout(true);
       });
 
+    // reset cart and totalprice after order is placed
 
-      // reset cart and totalprice after order is placed
-      
+    // then resset form values
+    setValues({
+      firstname: "",
 
-        // then resset form values
-        setValues({
-            firstname: "",
-
-            lastname: "",
-            email: "",
-            phone: "",
-            city: "",
-            adress: "",
-            state: "",
-            zip: "",
-            country: "",
-        });
+      lastname: "",
+      email: "",
+      phone: "",
+      city: "",
+      adress: "",
+      state: "",
+      zip: "",
+      country: "",
+    });
   };
 
-  return (
-    <div className=" pb-20">
-      <div className=" ml-14 mt-14 w-[350px]">
-        {/* -header- */}
 
+
+
+// make payment here
+
+const handleCheckout = async () => {
+  const stripe = await getStripe();
+
+  console.log(userdata?.cart);
+
+
+  const response = await fetch('/api/stripe', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userdata?.cart),
+  });
+  if(response.statusCode === 500) return;
+  
+  const data = await response.json();
+
+  await updateDoc(doc(db, "Users", `${userinfo?.email}`), {
+      cart: [],
+      totalprice: 0,
+  }
+  );
+  console.log('response Data------------>', data)
+
+//   toast.loading('Redirecting...');
+
+  stripe.redirectToCheckout({ sessionId: data.id });
+}
+
+
+
+
+
+
+
+  return (
+    <div className=" pb-20 grid grid-cols-12">
+      {/* // form--- */}
+
+      <div className=" sm:col-span-12 lg:col-span-7 ml-14 mt-14 w-[350px]">
+        {/* -header- */}
+        {orderinfo?.email}
         <div className=" relative">
           <div className=" absolute  top-[-29px] lg:left-[364px]">
             <h1>Shipping Details {userdata?.cart?.length}</h1>
@@ -138,6 +184,29 @@ const Step2 = () => {
                       type="text"
                       placeholder="Doe"
                     />
+                  </div>
+
+                  {/* -email- */}
+
+                  <div className="w-full md:w-1/2 px-3 mb-[2px] md:mb-0">
+                    <label
+                      className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                      htmlFor="email-id"
+                    >
+                      Email
+                    </label>
+                    <input
+                      onChange={handleInputChange}
+                      name="email"
+                      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                      id="email-id"
+                      type="email"
+                      placeholder="Jane"
+                    />
+
+                    <p className="text-red-500 text-xs italic">
+                      Please fill out this field.
+                    </p>
                   </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
@@ -221,6 +290,106 @@ const Step2 = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      {/* // order data--- */}
+
+      <div className=" mt-20  sm:col-span-12 ml-12 mr-12 lg:col-span-5">
+        {ordercheckout ? (
+          <div>
+            {/* ---header--- */}
+
+            <div>
+              <h1 className="text-2xl mb-4 p-2 w-2/3 bg-green-400 font-bold text-gray-800">
+                Order Details
+              </h1>
+            </div>
+
+            {/* ---info--- */}
+
+            <div>
+              <div className=" flex gap-2 mx-2">
+                <p className="  font-bold ">Products Number</p>{" "}
+                <p>{userdata?.cart?.length}</p>
+              </div>
+
+              {/* ----customer data--- */}
+
+              <div>
+                <div className=" my-4">
+                  <h1 className=" my-2 flex gap-2">
+                    {" "}
+                    <p className=" font-bold mr-2">Email:</p>{" "}
+                    <p>{userdata?.email}</p>
+                  </h1>
+
+                  <h1 className=" flex gap-2 my-2">
+                    {" "}
+                    <p className=" font-bold mr-2">City:</p>
+                    <p>{orderinfo?.city}</p>
+                  </h1>
+
+                  <h1 className=" flex gap-2 my-2">
+                    {" "}
+                    <p className=" font-bold mr-2">Adress:</p>
+                    <p>{orderinfo?.adress}</p>
+                  </h1>
+
+                  <h1 className=" flex gap-2 my-2">
+                    {" "}
+                    <p className=" font-bold mr-2">Phone:</p>
+                    <p>{orderinfo?.phone}</p>
+                  </h1>
+
+                  <h1 className=" flex gap-2 my-2">
+                    {" "}
+                    <p className=" font-bold mr-2">Zip:</p>
+                    <p>{orderinfo?.zip}</p>
+                  </h1>
+
+                  <h1 className=" flex gap-2 my-2">
+                    {" "}
+                    <p className=" font-bold mr-2">Total Price:</p>
+                    <p>{orderinfo?.total}</p>
+                  </h1>
+
+                  <div>
+                    <div>
+                      <button
+                      onClick={handleCheckout}
+                        type="button"
+                        className="text-gray-900 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 mr-2 mb-2"
+                      >
+                        <svg
+                          class="w-4 h-4 mr-2 -ml-1"
+                          aria-hidden="true"
+                          focusable="false"
+                          data-prefix="fab"
+                          data-icon="paypal"
+                          role="img"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 384 512"
+                        >
+                          <path
+                            fill="currentColor"
+                            d="M111.4 295.9c-3.5 19.2-17.4 108.7-21.5 134-.3 1.8-1 2.5-3 2.5H12.3c-7.6 0-13.1-6.6-12.1-13.9L58.8 46.6c1.5-9.6 10.1-16.9 20-16.9 152.3 0 165.1-3.7 204 11.4 60.1 23.3 65.6 79.5 44 140.3-21.5 62.6-72.5 89.5-140.1 90.3-43.4 .7-69.5-7-75.3 24.2zM357.1 152c-1.8-1.3-2.5-1.8-3 1.3-2 11.4-5.1 22.5-8.8 33.6-39.9 113.8-150.5 103.9-204.5 103.9-6.1 0-10.1 3.3-10.9 9.4-22.6 140.4-27.1 169.7-27.1 169.7-1 7.1 3.5 12.9 10.6 12.9h63.5c8.6 0 15.7-6.3 17.4-14.9 .7-5.4-1.1 6.1 14.4-91.3 4.6-22 14.3-19.7 29.3-19.7 71 0 126.4-28.8 142.9-112.3 6.5-34.8 4.6-71.4-23.8-92.6z"
+                          ></path>
+                        </svg>
+                        Check out with PayPal
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <h1 className=" font-bold text-2xl text-red-500 ">
+              You have no order yet
+            </h1>
+          </div>
+        )}
       </div>
     </div>
   );
